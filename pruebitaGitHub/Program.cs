3572c0d5d79792d274c7421388d0f7e1bd4dd069
mlopezg9 <81184097+mlopezg9@users.noreply.github.com>
2021-12-28 17:51:40 +0100

    Initial commit

---
+++ pruebitaGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pruebitaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
