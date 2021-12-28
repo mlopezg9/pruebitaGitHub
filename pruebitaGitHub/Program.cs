290517005f99948dd92a43405dea1d4920dba1ec
mariacaballeromone <96788540+mariacaballeromone@users.noreply.github.com>
2021-12-28 18:12:34 +0100

    <3

--- pruebitaGitHub/Program.cs
+++ pruebitaGitHub/Program.cs
@@ -6,7 +6,7 @@ namespace pruebitaGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("uwu");
         }
     }
 }
